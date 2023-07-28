d2e7a3cc457c8135e40547cf27c88cde148529fd
Arti Kumari <107554479+1305artikumari@users.noreply.github.com>
2023-07-28 20:20:08 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,13 @@
+// var item1=document.querySelectorAll('.list-group-item:nth-child(odd)');
+// for(var i=0;i<item1.length;i++)
+// {
+//     item1[i].style.backgroundColor = 'green';
+// }
+// var item1=document.querySelectorAll('.list-group-item');
+// item1[1].style.color='green';
+
+var item=document.querySelector('.list-group-item:nth-child(2)');
+item.style.backgroundColor = 'green';
+
+var item2=document.querySelector('.list-group-item:nth-child(3)')
+item2.style.display = 'none';
